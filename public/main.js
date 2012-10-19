f2d3646fa0ccc2e8b10e6d9669f1a52030463d61
mode80 <github@tihan.com>
2012-10-18 18:00:40 -0700

    make indents two spaces

--- public/main.js
+++ public/main.js
@@ -1,31 +1,31 @@
 function bodyController ($scope) {
 
-    // Die "class"
-    function Die(value) {
-        this.value = value
-        this.selected = false
-    }
-    Die.prototype.roll = function roll(){
-        this.value = Math.ceil(Math.random()*6)
-    }
+  // Die "class"
+  function Die(value) {
+    this.value = value
+    this.selected = false
+  }
+  Die.prototype.roll = function roll(){
+    this.value = Math.ceil(Math.random()*6)
+  }
 
-    // Make array of dice with default
-    $scope.dice = []
-    for (var i = 1; i<=5; i++) $scope.dice.push(new Die(i))
+  // Make array of dice with default
+  $scope.dice = []
+  for (var i = 1; i<=5; i++) $scope.dice.push(new Die(i))
 
-    $scope.rollSelected = function() {
-        var selected = _.filter($scope.dice, function(die) { return die.selected })
-        _.each(selected, function(die) { die.roll() } )
+  $scope.rollSelected = function() {
+    var selected = _.filter($scope.dice, function(die) { return die.selected })
+    _.each(selected, function(die) { die.roll() } )
 
-    }
+  }
 
-    $scope.selectAll = function() {
-        _.each($scope.dice, function(die) {die.selected=true})
-    }
+  $scope.selectAll = function() {
+    _.each($scope.dice, function(die) {die.selected=true})
+  }
 
-    $scope.selectNone = function() {
-        _.each($scope.dice, function(die) {die.selected=false})
-    }
+  $scope.selectNone = function() {
+    _.each($scope.dice, function(die) {die.selected=false})
+  }
 
 }
 
