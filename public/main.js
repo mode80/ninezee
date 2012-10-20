b737c34f88bd47b3796d85a8f9602716a5070ab9
mode80 <mode80>
2012-10-20 10:23:30 -0700

    ace score proposal and lock working

--- public/main.js
+++ public/main.js
@@ -2,66 +2,80 @@ function bodyController ($scope) {
 
   // Die "class"
   function Die(value) {
-    this.value = value;
-    this.selected = false;
+    this.value = value
+    this.selected = false
   }
   Die.prototype.roll = function roll(){
-    this.value = Math.ceil(Math.random() * 6);
+    this.value = Math.ceil(Math.random() * 6)
   }
 
   // ScoreBox "class"
   function ScoreBox() {
-    this.val = null;
-    this.maybeVal = null;
-    };
+    this.val = null
+    this.temporary = false
+    }
   ScoreBox.prototype.calcVal = function (dieArray) {
     // override this
+  }
+  ScoreBox.prototype.proposeVal = function(dieArray) {
+    if (this.val == null) {
+      this.val=this.calcVal(dieArray)
+      this.temporary = true
+    }
+  }
+  ScoreBox.prototype.unproposeVal = function(dieArray) {
+    if (this.temporary) this.val=null
+    this.temporary = false
+  }
+  ScoreBox.prototype.lockVal = function(dieArray) {
+    this.val = this.calcVal(dieArray)
+    this.temporary = false
   }
 
   // SimpleScoreBox "class"
   function SimpleScoreBox(n) {
-    this.n = n;
-    };
-  SimpleScoreBox.prototype = new ScoreBox();
+    this.n = n
+    }
+  SimpleScoreBox.prototype = new ScoreBox()
   SimpleScoreBox.prototype.calcVal = function (dieArray) {
-    var sum = 0;
+    var sum = 0
     for (var i= 0, len=dieArray.length; i < len; i++) {
       if (this.n === dieArray[i].value) 
-        sum = sum + dieArray[i].value;
+        sum = sum + dieArray[i].value
     }
-    return sum;
+    return sum
   }
 
   // Player "class"
   function Player(name) {
-    this.name = name || "Player";
-    this.aces    = new SimpleScoreBox(1);
-    this.twos    = new SimpleScoreBox(2);
-    this.threes  = new SimpleScoreBox(3);
-    this.fours   = new SimpleScoreBox(4);
-    this.fives   = new SimpleScoreBox(5);
-    this.sixes   = new SimpleScoreBox(6);
+    this.name = name || "Player"
+    this.aces    = new SimpleScoreBox(1)
+    this.twos    = new SimpleScoreBox(2)
+    this.threes  = new SimpleScoreBox(3)
+    this.fours   = new SimpleScoreBox(4)
+    this.fives   = new SimpleScoreBox(5)
+    this.sixes   = new SimpleScoreBox(6)
   }
 
   // Make array of dice with default
-  $scope.dice = [];
-  for (var i = 1; i<=5; i++) $scope.dice.push(new Die(i));
+  $scope.dice = []
+  for (var i = 1; i<=5; i++) $scope.dice.push(new Die(i))
 
   // Init other model objects
-  $scope.p1 = new Player("Player #1");
+  $scope.p1 = new Player("Player #1")
 
   $scope.rollSelected = function() {
-    var selected = _.filter($scope.dice, function(die) { return die.selected; });
-    _.each(selected, function(die) { die.roll() } );
+    var selected = _.filter($scope.dice, function(die) { return die.selected; })
+    _.each(selected, function(die) { die.roll() } )
 
   }
 
   $scope.selectAll = function() {
-    _.each($scope.dice, function(die) {die.selected=true});
+    _.each($scope.dice, function(die) {die.selected=true})
   }
 
   $scope.selectNone = function() {
-    _.each($scope.dice, function(die) {die.selected=false});
+    _.each($scope.dice, function(die) {die.selected=false})
   }
 
 }
